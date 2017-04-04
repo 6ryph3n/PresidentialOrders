932d8015c1cb093814a4579518efb2db0db79436
Steven Beckham <stevenbeckham@stevens-macbook-pro.local>
2017-04-04 14:51:30 -0400

    Updated data handling logic to retain both "name" and "inauguration" attributes when repopulating fields

--- Presidential Orders/ViewController.swift
+++ Presidential Orders/ViewController.swift
@@ -8,23 +8,29 @@
 
 import UIKit
 
+var workingSet = NewSet()
+var president1 = workingSet.0
+var president2 = workingSet.1
+var president3 = workingSet.2
+var president4 = workingSet.3
+
 class ViewController: UIViewController {
     @IBOutlet weak var Field1: UILabel!
     @IBOutlet weak var Field2: UILabel!
     @IBOutlet weak var Field3: UILabel!
     @IBOutlet weak var Field4: UILabel!
-    var workingSet = NewSet()
-    
+
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
         
-        Field1.text = workingSet.0.name
-        Field2.text = workingSet.1.name
-        Field3.text = workingSet.2.name
-        Field4.text = workingSet.3.name
+        Field1.text = president1.name
+        Field2.text = president2.name
+        Field3.text = president3.name
+        Field4.text = president4.name
         
     }
     
@@ -37,39 +43,52 @@ class ViewController: UIViewController {
     // Up and Down Buttons
     
     @IBAction func field1_down(_ sender: Any) {
-        let transferPlaceholder = Field2.text
-        Field2.text = Field1.text
-        Field1.text = transferPlaceholder
+        
+        let transferPlaceholder = president2
+        president2 = president1
+        president1 = transferPlaceholder
+        Field1.text = president1.name
+        Field2.text = president2.name
     }
     
     @IBAction func field2_up(_ sender: Any) {
-        let transferPlaceholder = Field1.text
-        Field1.text = Field2.text
-        Field2.text = transferPlaceholder
+        let transferPlaceholder = president1
+        president1 = president2
+        president2 = transferPlaceholder
+        Field1.text = president1.name
+        Field2.text = president2.name
     }
     
     @IBAction func field2_down(_ sender: Any) {
-        let transferPlaceholder = Field3.text
-        Field3.text = Field2.text
-        Field2.text = transferPlaceholder
+        let transferPlaceholder = president3
+        president3 = president2
+        president2 = transferPlaceholder
+        Field2.text = president2.name
+        Field3.text = president3.name
     }
     
     @IBAction func field3_up(_ sender: Any) {
-        let transferPlaceholder = Field2.text
-        Field2.text = Field3.text
-        Field3.text = transferPlaceholder
+        let transferPlaceholder = president2
+        president2 = president3
+        president3 = transferPlaceholder
+        Field2.text = president2.name
+        Field3.text = president3.name
     }
     
     @IBAction func field3_down(_ sender: Any) {
-        let transferPlaceholder = Field4.text
-        Field4.text = Field3.text
-        Field3.text = transferPlaceholder
+        let transferPlaceholder = president4
+        president4 = president3
+        president3 = transferPlaceholder
+        Field3.text = president3.name
+        Field4.text = president4.name
     }
     
     @IBAction func field4_up(_ sender: Any) {
-        let transferPlaceholder = Field3.text
-        Field3.text = Field4.text
-        Field4.text = transferPlaceholder
+        let transferPlaceholder = president3
+        president3 = president4
+        president4 = transferPlaceholder
+        Field3.text = president3.name
+        Field4.text = president4.name
     }
     
     
